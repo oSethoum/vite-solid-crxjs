4f162101bdbf183a7db8fe3554f028f675e83c49
Oussama Sethoum <os.sethoum@gmail.com>
2023-02-06 04:54:36 +0100

    initial

---
+++ src/content.tsx
@@ -0,0 +1,11 @@
+/* @refresh reload */
+import { render } from "solid-js/web";
+
+import "./index.css";
+import App from "./App";
+
+const root = document.createElement("div");
+root.id = "crx-root";
+document.body.append(root);
+
+render(() => <App />, root!);
